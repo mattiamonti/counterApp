326c578e46108558b90aedf3c9b619b3014532e8
mattiamonti <mattiamonti2001@gmail.com>
2021-07-02 23:03:17 +0200

    25

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
+import { useRef } from 'react';
+import { useState } from 'react';
 import './App.css';
+import TodoList from './components/TodoList';
 
 function App() {
+  
+  const [count1, setCount1] = useState(0);
+  const [count2, setCount2] = useState(0);
+  const [count3, setCount3] = useState(0);
+
+  function incr1 () {
+    setCount1(count1+1)
+  }
+  function decr1 () {
+    setCount1(count1-1)
+  }
+
+  function incr2 () {
+    setCount2(count2+1)
+  }
+  function decr2 () {
+    setCount2(count2-1)
+  }
+
+  function incr3 () {
+    setCount3(count3+1)
+  }
+  function decr3 () {
+    setCount3(count3-1)
+  }
+
+
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  
+      <div className='card1'>
+        <h2>Counter 1</h2>
+        <div>
+        <button onClick={decr1}>-</button>
+        <span>{count1}</span>
+        <button onClick={incr1}>+</button>
+        </div>
+
+      </div>
+
+      <div className='card2'>
+        <h2>Counter 2</h2>
+        <div>
+        <button onClick={decr2}>-</button>
+        <span>{count2}</span>
+        <button onClick={incr2}>+</button>
+        </div>
+
+      </div>
+
+      <div className='card3'>
+        <h2>Counter 3</h2>
+        <div>
+        <button onClick={decr3}>-</button>
+        <span>{count3}</span>
+        <button onClick={incr3}>+</button>
+        </div>
+
+      </div>
+
     </div>
   );
 }
---
+++ src/components/Todo.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Todo({todo}) {
+
+    return (
+        <div>
+            <label>
+                <input type = 'checkbox' checked={todo.complete} />
+                {todo.name}
+            </label>
+        </div>
+    );
+
+}
+
+export default Todo;
