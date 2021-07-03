7ba6426d03a58e6c342fbb8b952af65b7b443fc2
mattiamonti <mattiamonti2001@gmail.com>
2021-07-03 16:53:54 +0200

    4

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useRef } from 'react';
+
 import { useState, useEffect} from 'react';
 import './App.css';
 
@@ -60,7 +60,7 @@ function App() {
           <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-8.707l-3-3a1 1 0 00-1.414 1.414L10.586 9H7a1 1 0 100 2h3.586l-1.293 1.293a1 1 0 101.414 1.414l3-3a1 1 0 000-1.414z" clipRule="evenodd" />
         </svg>
         </div>
-        
+
         <svg onClick={reset1} xmlns="http://www.w3.org/2000/svg" class="resetButton" fill="none" viewBox="0 0 24 24" stroke="currentColor">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2M3 12l6.414 6.414a2 2 0 001.414.586H19a2 2 0 002-2V7a2 2 0 00-2-2h-8.172a2 2 0 00-1.414.586L3 12z" />
         </svg>
